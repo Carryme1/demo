4271b377084f133c60970b25c93d6585a6ca960e
nongnong <862569145@qq.com>
2021-01-18 19:27:54 +0800

    frist

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+@PostMapping("/index")
+@ResponseBody
+    public String index(){
+    return "hello";
+}
+}
